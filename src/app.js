1fb80053df5e64b5f145e2aa5b7a13e5868592e4
jaydenpung <pungkahheng@gmail.com>
2019-05-14 23:23:58 +0800

    Reconnect imap on every run

--- src/app.js
+++ src/app.js
@@ -30,100 +30,99 @@ function openInbox(cb) {
 imap.once('ready', function () {
 
     const dir = './pdf';
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
 
     openInbox(function (err, box) {
         if (err) throw err;
-        repeat();
-        setInterval(repeat, 30 * 60 * 1000);
+        imap.search(['UNSEEN', ['FROM', 'm2u@bills.maybank2u.com.my']], function (err, results) {
+            if (err || !results.length) return;
 
-        function repeat() {
-            imap.search(['UNSEEN', ['FROM', 'm2u@bills.maybank2u.com.my']], function (err, results) {
-                if (err || !results.length) return;
+            //mark as read
+            imap.setFlags(results, ['\\Seen'], function (err) {
+                if (!err) {
+                    console.log("marked as read");
+                } else {
+                    console.log(JSON.stringify(err, null, 2));
+                }
+            });
 
-                //mark as read
-                imap.setFlags(results, ['\\Seen'], function (err) {
-                    if (!err) {
-                        console.log("marked as read");
-                    } else {
-                        console.log(JSON.stringify(err, null, 2));
+            // fetch all resulting messages
+            let f = imap.fetch(results, {
+                bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+                struct: true
+            });
+            f.on('message', function (msg, seqno) {
+                console.log('Message #%d', seqno);
+                let prefix = '(#' + seqno + ') ';
+                msg.on('body', function (stream, info) {
+                    let buffer = '';
+                    stream.on('data', function (chunk) {
+                        buffer += chunk.toString('utf8');
+                    });
+                    stream.once('end', function () {
+                        console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+                    });
+                });
+                msg.once('attributes', function (attrs) {
+                    let attachments = findAttachmentParts(attrs.struct);
+                    console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+                    for (let i = 0, len = attachments.length; i < len; ++i) {
+                        let attachment = attachments[i];
+                        /*This is how each attachment looks like {
+                            partID: '2',
+                            type: 'application',
+                            subtype: 'octet-stream',
+                            params: { name: 'file-name.ext' },
+                            id: null,
+                            description: null,
+                            encoding: 'BASE64',
+                            size: 44952,
+                            md5: null,
+                            disposition: { type: 'ATTACHMENT', params: { filename: 'file-name.ext' } },
+                            language: null
+                          }
+                        */
+                        console.log(prefix + 'Fetching attachment %s', attachment.params.name);
+                        let f = imap.fetch(attrs.uid, { //do not use imap.seq.fetch here
+                            bodies: [attachment.partID],
+                            struct: true
+                        });
+                        //build function to process attachment message
+                        attachment.date = attrs.date;
+                        f.on('message', buildAttMessageFunction(attachment));
                     }
                 });
-
-                // fetch all resulting messages
-                let f = imap.fetch(results, {
-                    bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-                    struct: true
+                msg.once('end', function () {
+                    console.log(prefix + 'Finished');
                 });
-                f.on('message', function (msg, seqno) {
-                    console.log('Message #%d', seqno);
-                    let prefix = '(#' + seqno + ') ';
-                    msg.on('body', function (stream, info) {
-                        let buffer = '';
-                        stream.on('data', function (chunk) {
-                            buffer += chunk.toString('utf8');
-                        });
-                        stream.once('end', function () {
-                            console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-                        });
-                    });
-                    msg.once('attributes', function (attrs) {
-                        let attachments = findAttachmentParts(attrs.struct);
-                        console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-                        for (let i = 0, len = attachments.length; i < len; ++i) {
-                            let attachment = attachments[i];
-                            /*This is how each attachment looks like {
-                                partID: '2',
-                                type: 'application',
-                                subtype: 'octet-stream',
-                                params: { name: 'file-name.ext' },
-                                id: null,
-                                description: null,
-                                encoding: 'BASE64',
-                                size: 44952,
-                                md5: null,
-                                disposition: { type: 'ATTACHMENT', params: { filename: 'file-name.ext' } },
-                                language: null
-                              }
-                            */
-                            console.log(prefix + 'Fetching attachment %s', attachment.params.name);
-                            let f = imap.fetch(attrs.uid, { //do not use imap.seq.fetch here
-                                bodies: [attachment.partID],
-                                struct: true
-                            });
-                            //build function to process attachment message
-                            attachment.date = attrs.date;
-                            f.on('message', buildAttMessageFunction(attachment));
-                        }
-                    });
-                    msg.once('end', function () {
-                        console.log(prefix + 'Finished');
-                    });
-                });
-                f.once('error', function (err) {
-                    console.log('Fetch error: ' + err);
-                });
-                f.once('end', function () {
-                    console.log('Done fetching all messages!');
-                });
-
             });
-        }
-
+            f.once('error', function (err) {
+                console.log('Fetch error: ' + err);
+                closeImap();
+            });
+            f.once('end', function () {
+                console.log('Done fetching all messages!');
+            });
+        });
     });
 });
 
 imap.once('error', function (err) {
     console.log(err);
+    closeImap();
 });
 
 imap.once('end', function () {
     console.log('Connection ended');
+    closeImap();
 });
 
 imap.connect();
+setInterval(() => {
+    imap.connect();
+}, 30 * 60 * 1000)
 
 function toUpper(thing) { return thing && thing.toUpperCase ? thing.toUpperCase() : thing; }
 
@@ -181,7 +180,8 @@ function processPdf(filename, fileDate) {
 
     decryptPdf(filename, newFilename)
         .then(() => sendFile(newFilename))
-        .then(() => removeFiles([filename, newFilename]));
+        .then(() => removeFiles([filename, newFilename]))
+        .then(() => closeImap());
 }
 
 function decryptPdf(filename, newFilename) {
@@ -218,23 +218,28 @@ function sendFile(newFilename) {
 
 function removeFiles(filenames) {
     return new Promise((resolve, reject) => {
-        deleteFiles(filenames, function(err) {
+        deleteFiles(filenames, function (err) {
             resolve();
-        });     
+        });
     });
 }
 
-function deleteFiles(files, callback){
+function deleteFiles(files, callback) {
     let i = files.length;
-    files.forEach(function(filepath){
-      fs.unlink(filepath, function(err) {
-        i--;
-        if (err) {
-          callback(err);
-          return;
-        } else if (i <= 0) {
-          callback(null);
-        }
-      });
+    files.forEach(function (filepath) {
+        fs.unlink(filepath, function (err) {
+            i--;
+            if (err) {
+                callback(err);
+                return;
+            } else if (i <= 0) {
+                callback(null);
+            }
+        });
     });
-  }
+}
+
+function closeImap() {
+    imap.destroy();
+    imap.end();
+}
